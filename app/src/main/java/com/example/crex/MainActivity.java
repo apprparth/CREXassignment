db395389b5e5146aa7480c8c9bea2edeb4ffa7bd
apurwapriyadarshini <apurwa.priyadarshini@parth.com>
2022-08-01 14:49:07 +0530

    1st Push

--- app/src/main/java/com/example/crex/MainActivity.java
+++ app/src/main/java/com/example/crex/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mainTabLayout));
     }
     private void setupTabsInTabLayout() {
         TabLayout.Tab upcomingTab = mainTabLayout.newTab();
